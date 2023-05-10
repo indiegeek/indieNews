ce3cb231d4e537c4bad89b34fff7166d49f4a56d
indiegeek <jsross@gmail.com>
2023-05-10 09:37:31 -0400

    started work on the array to carry topics to fetch and work on prepping calls to make began

--- app.js
+++ app.js
@@ -3,17 +3,42 @@ const env = require('dotenv').config();
 
 let apiKey = process.env.APIKEY
 
-var options = {
-  method: 'GET',
-  url: 'https://api.newscatcherapi.com/v2/search',
-  params: {q: 'Bitcoin', lang: 'en', sort_by: 'relevancy', page: '1'},
-  headers: {
-    'x-api-key': apiKey
-  }
-};
+var topics = ["puppies", "kittens", "fish", "chickens", "horses", "cows"]
 
-axios.request(options).then(function (response) {
-	console.log(response.data.articles[0].title);
-}).catch(function (error) {
-	console.error(error);
-});
+for (let i = 0; i < topics.length; i++) {
+    let currentTopic = topics[i]
+    console.log(currentTopic)
+    let makeCalls = `options = {
+        method: 'GET',
+        url: 'https://api.newscatcherapi.com/v2/search',
+        params: {q: '${ currentTopic }', lang: 'en', sort_by: 'relevancy', page: '1'},
+        headers: {
+            'x-api-key': ${ apiKey } 
+        };`
+        console.log(makeCalls);
+        }    
+
+// var options = {
+//   method: 'GET',
+//   url: 'https://api.newscatcherapi.com/v2/search',
+//   params: {q: 'Bitcoin', lang: 'en', sort_by: 'relevancy', page: '1'},
+//   headers: {
+//     'x-api-key': apiKey
+//   }
+// };
+// }
+
+// var options = {
+//   method: 'GET',
+//   url: 'https://api.newscatcherapi.com/v2/search',
+//   params: {q: 'Bitcoin', lang: 'en', sort_by: 'relevancy', page: '1'},
+//   headers: {
+//     'x-api-key': apiKey
+//   }
+// };
+
+// axios.request(options).then(function (response) {
+// 	console.log(response.data.articles[0].title);
+// }).catch(function (error) {
+// 	console.error(error);
+// });
