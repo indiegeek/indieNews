af8fd4a1220215e392535155cd181d557dfe947e
indiegeek <jsross@gmail.com>
2023-05-11 10:11:45 -0400

    built endpoint to add new topics to our db, added babel,added body-parser

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const env = require('dotenv').config();
 const express = require('express');
 const db = require('diskdb');
-
+const bodyParser = require('body-parser')
 //app vars
 const server = express();
 let apiKey = process.env.APIKEY //up to 10,000 calls/day for 90 days
@@ -36,14 +36,38 @@ for (let i = 0; i < topics.length; i++) {
         };`
         // console.log(makeCalls);
         }    
-
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({extended: false}))
 server.get("/", (req, res) => {
     res.sendFile(__dirname + '/html/index.html');
 });
-
 server.listen(port, () => {
     console.log(`Server listening at ${port}`);
 });
+server.post("/api/v1/addNewTopic", (req, res) => {
+    if(!req.body.topic) {
+        return res.status(400).send({
+          success: 'false',
+          message: 'topic is required'
+        });
+      } else if(!req.body.lang) {
+        return res.status(400).send({
+          success: 'false',
+          message: 'lang is required'
+        });
+      } 
+      const newTopic = {
+        id: db.topics.find().length+1,
+        topic: req.body.topic,
+        lang: req.body.lang
+      }
+      db.topics.save(newTopic);
+      return res.status(200).send({
+        success: true,
+        message: "Topic added successfully",
+        newTopic
+      })
+})
 // var options = {
 //   method: 'GET',
 //   url: 'https://api.newscatcherapi.com/v2/search',
